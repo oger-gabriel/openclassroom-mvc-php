5062da685419565213e51ce6c975177b690d29eb
oger-gabriel <40660493+0x4c756e61@users.noreply.github.com>
2024-10-22 10:10:42 +0200

    Créez un template et un layout

--- blog/index.php
+++ blog/index.php
@@ -1,7 +1,35 @@
 <?php
 
-require('src/model.php');
+require_once('src/controllers/add_comment.php');
+require_once('src/controllers/homepage.php');
+require_once('src/controllers/post.php');
 
-$posts = getPosts();
+try {
+    if (isset($_GET['action']) && $_GET['action'] !== '') {
+        if ($_GET['action'] === 'post') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                $identifier = $_GET['id'];
 
-require('templates/homepage.php');
+                post($identifier);
+            } else {
+                throw new Exception('Aucun identifiant de billet envoyé');
+            }
+        } elseif ($_GET['action'] === 'addComment') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                $identifier = $_GET['id'];
+
+                addComment($identifier, $_POST);
+            } else {
+                throw new Exception('Aucun identifiant de billet envoyé');
+            }
+        } else {
+            throw new Exception("La page que vous recherchez n'existe pas.");
+        }
+    } else {
+        homepage();
+    }
+} catch (Exception $e) {
+    $errorMessage = $e->getMessage();
+
+    require('templates/error.php');
+}
--- blog/src/model.php
+++ blog/src/model.php
@@ -1,18 +1,17 @@
 <?php
 
-function getPosts()
-{
+function getPosts() {
     $database = dbConnect();
     $statement = $database->query(
         "SELECT id, title, content, DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM posts ORDER BY creation_date DESC LIMIT 0, 5"
     );
     $posts = [];
-    while ($row = $statement->fetch()) {
+    while (($row = $statement->fetch())) {
         $post = [
-            "title" => $row["title"],
-            "french_creation_date" => $row["french_creation_date"],
-            "content" => $row["content"],
-            "identifier" => $row["id"],
+            'title' => $row['title'],
+            'french_creation_date' => $row['french_creation_date'],
+            'content' => $row['content'],
+            'identifier' => $row['id'],
         ];
 
         $posts[] = $post;
@@ -21,8 +20,7 @@ function getPosts()
     return $posts;
 }
 
-function getPost($identifier)
-{
+function getPost($identifier) {
     $database = dbConnect();
     $statement = $database->prepare(
         "SELECT id, title, content, DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM posts WHERE id = ?"
@@ -31,43 +29,18 @@ function getPost($identifier)
 
     $row = $statement->fetch();
     $post = [
-        "title" => $row["title"],
-        "french_creation_date" => $row["french_creation_date"],
-        "content" => $row["content"],
+        'title' => $row['title'],
+        'french_creation_date' => $row['french_creation_date'],
+        'content' => $row['content'],
+        'identifier' => $row['id'],
     ];
 
     return $post;
 }
 
-function getComments($identifier)
-{
-    $database = dbConnect();
-    $statement = $database->prepare(
-        "SELECT id, author, comment, DATE_FORMAT(comment_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM comments WHERE post_id = ? ORDER BY comment_date DESC"
-    );
-    $statement->execute([$identifier]);
-
-    $comments = [];
-    while ($row = $statement->fetch()) {
-        $comment = [
-            "author" => $row["author"],
-            "french_creation_date" => $row["french_creation_date"],
-            "comment" => $row["comment"],
-        ];
-
-        $comments[] = $comment;
-    }
-
-    return $comments;
-}
-
 function dbConnect()
 {
-    try {
-        $database = new PDO("mysql:host=127.0.0.1;dbname=blog;charset=utf8", "blog", "password");
+    $database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'blog', 'password');
 
-        return $database;
-    } catch (Exception $e) {
-        die("Erreur : " . $e->getMessage());
-    }
+    return $database;
 }
