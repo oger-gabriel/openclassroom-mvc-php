10de6596e561a81b18ddf8db64be850e9fd7d65a
oger-gabriel <40660493+0x4c756e61@users.noreply.github.com>
2024-10-22 09:04:40 +0200

    Mettez à jour le modèle

--- blog/src/model.php
+++ blog/src/model.php
@@ -1,9 +1,14 @@
 <?php
 
+function dbConnection()
+{
+    return new PDO("mysql:host=127.0.0.1;dbname=blog;charset=utf8", "blog", "password");
+}
+
 function getPosts()
 {
     try {
-        $database = new PDO("mysql:host=127.0.0.1;dbname=blog;charset=utf8", "blog", "password");
+        $database = dbConnection();
     } catch (Exception $e) {
         die("Erreur : " . $e->getMessage());
     }
@@ -24,4 +29,53 @@ function getPosts()
     }
 
     return $posts;
+}
+
+function getPost($identifier)
+{
+    try {
+        $database = dbConnection();
+    } catch (Exception $e) {
+        die("Erreur : " . $e->getMessage());
+    }
+
+    $statement = $database->prepare(
+        "SELECT id, title, content,
+DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss')
+AS french_creation_date FROM posts WHERE id = ?"
+    );
+    $statement->execute([$identifier]);
+    $row = $statement->fetch();
+    $post = [
+        "title" => $row["title"],
+        "french_creation_date" => $row["french_creation_date"],
+        "content" => $row["content"],
+    ];
+    return $post;
+}
+
+function getComments($identifier)
+{
+    try {
+        $database = dbConnection();
+    } catch (Exception $e) {
+        die("Erreur : " . $e->getMessage());
+    }
+    $statement = $database->prepare(
+        "SELECT id, author, comment,
+DATE_FORMAT(comment_date, '%d/%m/%Y à %Hh%imin%ss')
+AS french_creation_date FROM comments WHERE post_id = ?
+ORDER BY comment_date DESC"
+    );
+    $statement->execute([$identifier]);
+    $comments = [];
+    while ($row = $statement->fetch()) {
+        $comment = [
+            "author" => $row["author"],
+            "french_creation_date" => $row["french_creation_date"],
+            "comment" => $row["comment"],
+        ];
+        $comments[] = $comment;
+    }
+    return $comments;
 }
